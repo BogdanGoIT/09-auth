56427afd3fd8ed9d781e2ac3416a9e6af288541f
Dictator <dictatorbogdan@gmail.com>
2026-05-19 15:51:33 +0300

    add route protection in proxy.ts

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,10 +1,31 @@
-import type { Metadata } from 'next';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'Authorization | NoteHub',
-  description: 'Sign in and sign up page for the NoteHub system',
+// import type { Metadata } from 'next';
+import { useRouter } from 'next/navigation';
+import { startTransition, useEffect, useState } from 'react';
+
+// export const metadata: Metadata = {
+//   title: 'Authorization | NoteHub',
+//   description: 'Sign in and sign up page for the NoteHub system',
+// };
+
+type Props = {
+  children: React.ReactNode;
 };
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+export default function PublicLayout({ children }: Props) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    // 1. Примусово оновлюємо серверні дані
+    router.refresh();
+
+    // 2. Безпечно оновлюємо стан loading
+    startTransition(() => {
+      setLoading(false);
+    });
+  }, [router]);
+
+  return <>{loading ? <div>Loading...</div> : children}</>;
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import Link from 'next/link';
 import css from './Home.module.css';
 import AuthNavigation from '../AuthNavigation/AuthNavigation';
+import { useAuthStore } from '@/lib/store/authStore';
 
 export default function Header() {
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
   return (
     <header className={css.header}>
       <Link href="/" aria-label="Home">
@@ -13,9 +17,11 @@ export default function Header() {
           <li>
             <Link href="/">Home</Link>
           </li>
-          <li>
-            <Link href="/notes/filter/all">Notes</Link>
-          </li>
+          {isAuthenticated && (
+            <li>
+              <Link href="/notes/filter/all">Notes</Link>
+            </li>
+          )}
           <AuthNavigation />
         </ul>
       </nav>
